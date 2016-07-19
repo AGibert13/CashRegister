ea56ab217a4ccef533d6f4c2b1345dc87df2d792
AGibert13 <aulauragibert@gmail.com>
2016-07-19 08:26:18 -0400

    Update cashreg.js

--- cashreg.js
+++ cashreg.js
@@ -14,3 +14,22 @@ cashRegister.add(0.45);
 
 //Show the total bill
 console.log('Your bill is '+cashRegister.total);
+
+// How I set up the cart-(Adrien)
+var item = [];
+var quantity = [];
+var price = [];
+function receipt(){
+	var itemTotal = [];
+	var groceryList =[];
+	var cart = [];
+	for (i = 0; i < item.length; i++){
+		itemTotal.push(quantity[i] * price[i]);
+		groceryList.push([quantity[i].toString(), item[i], ("= " + itemTotal[i].toString())]);
+		 cart.push(groceryList[i].join(" "));
+		console.log(cart[i]);
+	}
+	alert(cart.join("\n"));
+}
+}
+receipt();
